1e07e8db7d22f84467d319b3daf44de512320d62
larav1 <l.vitorinha09@gmail.com>
2025-01-14 18:58:27 -0300

    Atualização do desenvolvimento extra

--- 5. JavaScript 1/DesenvolvimentoExtra5.1/script.js
+++ 5. JavaScript 1/DesenvolvimentoExtra5.1/script.js
@@ -1,1 +1,15 @@
-console.log("teste");
+const titulo = document.getElementById("titulo");
+const link = document.querySelector("a");
+
+titulo.innerText = "Lista de Séries";
+console.log(link);
+
+const lista = document.getElementById("lista-ordenada");
+const itens = lista.getElementsByTagName("li");
+
+// Altera o texto dos itens individualmente
+itens[0].innerText = "Teacup";
+itens[1].innerText = "ER";
+itens[2].innerText = "This is us";
+
+console.log(itens); // Para verificar o resultado
