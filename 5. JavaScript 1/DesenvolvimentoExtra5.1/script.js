8fab29fb8f9e4ff3e13388d1f4520c2b80648467
larav1 <l.vitorinha09@gmail.com>
2025-01-15 18:03:01 -0300

    Atividade concluida

--- 5. JavaScript 1/DesenvolvimentoExtra5.1/script.js
+++ 5. JavaScript 1/DesenvolvimentoExtra5.1/script.js
@@ -1,15 +1,30 @@
+// Capturar os elementos
 const titulo = document.getElementById("titulo");
 const link = document.querySelector("a");
+const listaNaoOrdenada = document.getElementById("lista-nao-ordenada");
+const listaOrdenada = document.getElementById("lista-ordenada");
 
+// Adicionar texto ao título e ao link
 titulo.innerText = "Lista de Séries";
-console.log(link);
+link.innerText = "Visite o site Proz Educação";
+link.setAttribute("target", "_blank");
 
-const lista = document.getElementById("lista-ordenada");
-const itens = lista.getElementsByTagName("li");
+// Adicionar itens à lista não ordenada (sem links)
+listaNaoOrdenada.innerHTML = `
+  <li>Coraline</li>
+  <li>Abbott Elementary</li>
+  <li>Criminal Minds</li>
+`;
 
-// Altera o texto dos itens individualmente
-itens[0].innerText = "Teacup";
-itens[1].innerText = "ER";
-itens[2].innerText = "This is us";
+// Adicionar itens à lista ordenada (com links)
+listaOrdenada.innerHTML = `
+  <li><a href="https://www.adorocinema.com/series/serie-33469/" target="_blank">Teacup</a></li>
+  <li><a href="https://www.adorocinema.com/series/serie-22123/" target="_blank">9-1-1</a></li>
+  <li><a href="https://www.adorocinema.com/series/serie-31297/" target="_blank">Not Dead Yet</a></li>
+`;
 
-console.log(itens); // Para verificar o resultado
+// Verificar no console se tudo foi capturado corretamente
+console.log("Título:", titulo);
+console.log("Link principal:", link);
+console.log("Lista não ordenada:", listaNaoOrdenada);
+console.log("Lista ordenada:", listaOrdenada);
